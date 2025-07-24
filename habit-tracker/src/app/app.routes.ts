fe4709cee3e3cc20fb234a4ca79c0ab88b225eda
Sarah <shaswell@scottlogic.com>
2025-07-24 16:46:38 +0100

    Lazy load non-home components, add titles, add icon to nope component

--- habit-tracker/src/app/app.routes.ts
+++ habit-tracker/src/app/app.routes.ts
@@ -1,28 +1,30 @@
 import { Routes } from '@angular/router';
-import { BlobView } from './components/blob-view/blob-view';
-import { GridView } from './components/grid-view/grid-view';
 import { Nope } from './components/nope/nope';
 import { Home } from './components/home/home';
 
 const homeRoute = {
     path: '',
-    component: Home
+    component: Home,
+    title: 'Habit Tracker App'
 }
 
 // TODO: consider parameterising for specific habit guid?
 const blobRoute = {
     path: 'blob',
-    component: BlobView
+    loadComponent: () => import('./components/blob-view/blob-view').then(m => m.BlobView),
+    title: 'Blob'
 }
 
 const gridRoute = {
     path: 'grid',
-    component: GridView
+    loadComponent: () => import('./components/grid-view/grid-view').then(m => m.GridView),
+    title: 'Grid'
 }
 
 const nopeRoute = {
     path: '**',
-    component: Nope
+    component: Nope,
+    title: 'Here be dragons'
 }
 
 export const routes: Routes = [blobRoute, gridRoute, homeRoute, nopeRoute];
--- habit-tracker/src/app/components/nope/nope.ts
+++ habit-tracker/src/app/components/nope/nope.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nope',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './nope.html',
   styleUrl: './nope.scss'
 })
