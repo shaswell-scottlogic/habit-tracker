f16731554feb1858819b15d1c14ecfc4fec74ffc
Sarah <shaswell@scottlogic.com>
2025-07-29 17:15:13 +0100

    Add judgement, change padding

--- habit-tracker/src/app/components/habit/habit.ts
+++ habit-tracker/src/app/components/habit/habit.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { HabitService } from '../../services/habit-service';
 import { map, switchMap } from 'rxjs';
@@ -7,7 +7,7 @@ import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-habit',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet],
   templateUrl: './habit.html',
   styleUrl: './habit.scss'
 })
@@ -17,4 +17,13 @@ export class HabitView {
 
   id$ = this.activatedRoute.params.pipe(map(params => parseInt(params['id'])));
   habit = toSignal(this.id$.pipe(switchMap((id) => this.habitService.getHabit(id))));
+
+  judgement = computed(() => {
+    const days = this.habit()?.daysCompleted;
+    if( days && days.length > 5) {
+      return 'SMASHING IT';
+    }
+
+    return 'FAILING HORRIBLY';
+  });
 }
