5efa6f4f35989c97a77bd695c437fdcace0f6f4e
Sarah <shaswell@scottlogic.com>
2025-07-29 10:22:29 +0100

    Move use of route into Habit component

--- habit-tracker/src/app/components/habit/habit.ts
+++ habit-tracker/src/app/components/habit/habit.ts
@@ -1,5 +1,5 @@
-import { Component, inject, signal, Signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject, signal, Signal } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { HabitService } from '../../services/habit-service';
 
 @Component({
@@ -11,5 +11,32 @@ import { HabitService } from '../../services/habit-service';
 export class Habit {
   // do I get the route here and extract the ID or do I have the habit service know which one is current?
   private habitService = inject(HabitService);
-  private habit = this.habitService.getHabit(); // TODO: what is the type?
+  private habit = {}; // TODO: what is the type?
+  // set up current habit id using activated route
+  private activatedRoute = inject(ActivatedRoute);
+
+  currentHabit: Signal<number> = computed(() => {
+    // use activated route
+    console.log(this.activatedRoute);
+
+    return 2;
+  })
+
+  constructor() {
+    console.log('constructing habit-service');
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params);
+      // TODO: turn param into ID and use
+      this.habit = this.habitService.getHabit(3); // make it calculated?
+    });
+    // .pipe(
+    //   takeUntilDestroyed(),
+    //   map((params) => {
+    //     console.log('blarp');
+    //     console.log(params);
+    //     console.log(this.activatedRoute.url);
+    //     return params
+    //   })
+    // ).subscribe();
+  }
 }
--- habit-tracker/src/app/services/habit-service.ts
+++ habit-tracker/src/app/services/habit-service.ts
@@ -1,8 +1,6 @@
-import { computed, inject, Injectable, Signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Habit } from '../models/habit';
-import { map, of } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,50 +9,22 @@ export class HabitService {
   habitIds: number[] = [];
   habits: Map<number, Habit> = new Map();
 
-  // set up current habit id using activated route
-  private activatedRoute = inject(ActivatedRoute);
-
- // THIS SERVICE IS ONLY COMING INTO EXISTENCE WHEN ON HABIT PAGE?
- // how do nested routes manifest?
-
-  constructor() {
-    console.log('constructing habit-service');
-    this.activatedRoute.params.subscribe((params) => {
-      console.log(params)
-    });
-    // .pipe(
-    //   takeUntilDestroyed(),
-    //   map((params) => {
-    //     console.log('blarp');
-    //     console.log(params);
-    //     console.log(this.activatedRoute.url);
-    //     return params
-    //   })
-    // ).subscribe();
-  }
-
   getHabitIds() {
     return this.habitIds.length > 0 ? of(this.habitIds) : this.retrieveHabitIds();
   }
 
   private retrieveHabitIds() {
-    return of([1, 2, 3]);
+    return of([123, 212, 323]);
   }
-
-  currentHabit: Signal<number> = computed(() => {
-    // use activated route
-    console.log(this.activatedRoute);
-
-    return 2;
-  });
+;
 
   // is it ok to make an observable of something in a map?
-  getHabit(id?: number) {
-    const idToUse = id ?? this.currentHabit();
-    return this.habits.has(idToUse) ? of(this.habits.get(idToUse)) : this.retrieveHabit(idToUse)
+  getHabit(id: number) {
+    return this.habits.has(id) ? of(this.habits.get(id)) : this.retrieveHabit(id)
   }
 
+  // TODO: use httpResource?
   private retrieveHabit(id: number) {
-    return of({ name: '', daysCompleted: [new Date()] });
+    return of({ name: 'Janet', daysCompleted: [new Date()] });
   }
 }
