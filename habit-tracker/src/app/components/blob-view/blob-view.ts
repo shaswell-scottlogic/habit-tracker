df5bc7f8d83b5bc3732ce58d3b8a09c93d09ee34
Sarah <shaswell@scottlogic.com>
2025-07-30 17:03:27 +0100

    Pass something into tabbed components

--- habit-tracker/src/app/components/blob-view/blob-view.ts
+++ habit-tracker/src/app/components/blob-view/blob-view.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Blob } from '../blob/blob';
+import { HabitService } from '../../services/habit-service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-blob-view',
@@ -8,11 +10,34 @@ import { Blob } from '../blob/blob';
   styleUrl: './blob-view.scss'
 })
 export class BlobView {
+  // TOOD: consider making this dumber - have habit level own data
+  habitService = inject(HabitService);
+
+  @Input() id!: number;
+  slotData =  toSignal(this.habitService.getHabit(this.id));
+  
+  // TODO:
+  // get habit ID as input
+  // get habit data from service
+  // convert it to slot data
+  // then bulk it out - computed?
+
   slots = [
     { complete: true, date: new Date() },
     { complete: false, date: new Date() },
     { complete: true, date: new Date() },
     { complete: false, date: new Date() },
+    { complete: true, date: new Date() },
+    { complete: true, date: new Date() },
+    { complete: true, date: new Date() },
+    { complete: true, date: new Date() },
+    { complete: true, date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
     { date: new Date() },
   ];
+
+  
 }
--- habit-tracker/src/app/components/grid-view/grid-view.ts
+++ habit-tracker/src/app/components/grid-view/grid-view.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grid-view',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './grid-view.scss'
 })
 export class GridView {
-
+  @Input() id!: number;
 }
