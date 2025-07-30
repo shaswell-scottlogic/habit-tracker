3f7598006b8a793e82fcb0543bd9cc4188436b27
Sarah <shaswell@scottlogic.com>
2025-07-30 13:53:11 +0100

    Add home link to header

--- habit-tracker/src/app/components/header/header.ts
+++ habit-tracker/src/app/components/header/header.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ThemeService } from '../../services/theme';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSlideToggleModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSlideToggleModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
