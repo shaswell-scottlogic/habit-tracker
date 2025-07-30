b481a7dd970df3e4b1ee2de72df4d394f86c7dee
Sarah <shaswell@scottlogic.com>
2025-07-30 13:40:56 +0100

    Use withComponentInputBinding to get param into component from route

--- habit-tracker/src/app/components/habit/habit.ts
+++ habit-tracker/src/app/components/habit/habit.ts
@@ -1,9 +1,7 @@
-import { Component, computed, inject } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Component, computed, inject, Input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HabitService } from '../../services/habit-service';
-import { map, switchMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-habit',
@@ -13,10 +11,9 @@ import { AsyncPipe } from '@angular/common';
 })
 export class HabitView {
   habitService = inject(HabitService);
-  private activatedRoute = inject(ActivatedRoute);
 
-  id$ = this.activatedRoute.params.pipe(map(params => parseInt(params['id'])));
-  habit = toSignal(this.id$.pipe(switchMap((id) => this.habitService.getHabit(id))));
+  @Input() id!: number;
+  habit = toSignal(this.habitService.getHabit(this.id));
 
   judgement = computed(() => {
     const days = this.habit()?.daysCompleted;
