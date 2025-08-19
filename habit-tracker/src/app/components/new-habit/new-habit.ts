91eadb7e2346a3b0653380d5bc1e6c1ae8df3255
Sarah <shaswell@scottlogic.com>
2025-08-19 17:08:53 +0100

    Use form control instead of ngModel

--- habit-tracker/src/app/components/new-habit/new-habit.ts
+++ habit-tracker/src/app/components/new-habit/new-habit.ts
@@ -3,16 +3,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { HabitPattern } from '../../models/HabitPattern';
 import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-habit',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './new-habit.html',
   styleUrl: './new-habit.scss'
 })
 export class NewHabit {
   frequencyOptions = [HabitPattern.DAILY, HabitPattern.EVERY_OTHER, HabitPattern.EVERY_SEVEN];
-  enteredName = "";
-  selectedFrequency = null;
+
+  nameControl = new FormControl<string>("", Validators.required);
+  frequencyControl = new FormControl<HabitPattern | null>(null, Validators.required);
 }
