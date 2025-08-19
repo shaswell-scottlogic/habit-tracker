d0da712454c88bdb39ad7104a987ba5ec0797174
Sarah <shaswell@scottlogic.com>
2025-08-19 15:23:37 +0100

    Use mat select, add forms module, add text to reflect form values

--- habit-tracker/src/app/components/new-habit/new-habit.ts
+++ habit-tracker/src/app/components/new-habit/new-habit.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { HabitPattern } from '../../models/HabitPattern';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-habit',
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './new-habit.html',
   styleUrl: './new-habit.scss'
 })
 export class NewHabit {
   frequencyOptions = [HabitPattern.DAILY, HabitPattern.EVERY_OTHER, HabitPattern.EVERY_SEVEN];
+  enteredName = "";
+  selectedFrequency = null;
 }
