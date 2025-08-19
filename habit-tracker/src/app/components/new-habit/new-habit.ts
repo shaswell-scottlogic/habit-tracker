a4bd7809338e9f96537ad59050c1b285ed160221
Sarah <shaswell@scottlogic.com>
2025-08-19 11:52:25 +0100

    Add form with habit name and frequency fields; mix of mat and native

--- habit-tracker/src/app/components/new-habit/new-habit.ts
+++ habit-tracker/src/app/components/new-habit/new-habit.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HabitPattern } from '../../models/HabitPattern';
 
 @Component({
   selector: 'app-new-habit',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule],
   templateUrl: './new-habit.html',
   styleUrl: './new-habit.scss'
 })
 export class NewHabit {
-
+  frequencyOptions = [HabitPattern.DAILY, HabitPattern.EVERY_OTHER, HabitPattern.EVERY_SEVEN];
 }
